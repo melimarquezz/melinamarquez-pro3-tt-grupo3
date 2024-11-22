c6a9f8ad6038f1ff88438d755ad0e244debf547e
melimarquezz <mmelina@udesa.edu.ar>
2024-11-21 21:27:13 -0300

    armado de archivos

--- melinamarquez-pro3-tt-grupo3/App.js
+++ melinamarquez-pro3-tt-grupo3/App.js
@@ -3,7 +3,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Register from './src/screens/Register';
-
 /* Acordarme de importar las screens */
 
 const Stack = createNativeStackNavigator()
@@ -14,7 +13,6 @@ export default function App() {
     /* esto es si los usuarios están logueados */
     <NavigationContainer>
       <Stack.Navigator>
-
         <Stack.Screen name='Register' component={Register}/>
       </Stack.Navigator>
     </NavigationContainer>
